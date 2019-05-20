324d7e65a741ffbc11a5c5d635698111cacb45d5
Daniel Ang <dan.justin.ang@gmail.com>
2019-05-20 14:51:29 -0400

    initial commit, created react app (this application is a redux example to learn about how a store can interact with the react view and reduce to update and change state

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Post from './components/Post'
+import PostForm from './components/PostForm'
+
+function App() {
+  return (
+    <div className="App">
+      <PostForm/>
+      <hr />
+      <Post/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PostForm.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class PostForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Add Post</h1>
+      </div>
+    );
+  }
+}
+
+export default PostForm;
